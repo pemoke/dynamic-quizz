64004f20501f2d3f7e6b8a3c0b94100d5e70d19a
Peter Marko <pemoke@gmail.com>
2014-06-17 20:55:43 +1000

    Init commit

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+var allQuestions = [
+    {
+        question: 'Who is Prime Minister of the United Kingdom?',
+        choices: ['David Cameron', 'Gordon Brown', 'Winston Churchill', 'Tony Blair'],
+        correctAnswer: 0
+    },
+    {
+        question: 'Which pop duo was the first western band to play in The Peoples Republic of China?',
+        choices: ['Wham', 'Simon and Garfunkel', 'Chas and Dave', 'Right Said Fred'],
+        correctAnswer: 0
+    },
+    {
+        question: 'Timber selected from how many fully grown oak trees were needed to build a large 3 decker Royal Navy battle ship in the 18th century?',
+        choices: ['50', '500', '1500', '3500'],
+        correctAnswer: 0
+    }
+];
+
+$(function(){
+    console.log('loaded doc');
+});
