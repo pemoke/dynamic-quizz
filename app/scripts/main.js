785dd1270e2b4e878433180ae240f607d5855a19
Peter Marko <pemoke@gmail.com>
2014-06-21 23:22:06 +1000

    vanilla JavaScript (no jQuery) + Handlebars
    Rewritten in pure (vanilla) JavaScript.
    Removed jQuery.
    Added Handlebars.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,103 +2,153 @@
 
 var answers = [];
 
+
 /*
     Function to display the questions
  */
-function askQuestion(quizData, currentQuestion) {
-
-    $('#quiz').empty();
-
-    $('#quiz').append('<h3>' + quizData[currentQuestion].question + '</h3>');
-    for (var iChoice in quizData[currentQuestion].choices) {
-        var iChoiceVal = quizData[currentQuestion].choices[iChoice];
-        $('#quiz')
-            .append(
-                '<input type="radio" name="choices" id="choice' + iChoice + '" value="' + iChoice + '">' +
-                '<label for=choice' + iChoice + '>' + iChoiceVal + '</label><br>'
-        );
-    }
+function askQuestion(quizData, template, currentQuestion) {
 
     /*
-        If choice has been already made, show it (for ex. in case of Prev/Next navigation)
+        Populate Handlebars template
+     */
+    var context  = quizData[currentQuestion];
+    var html     = template(context);
+
+    document.getElementById('quiz').innerHTML = html;
+    // jQuery alternative:   $('#quiz').html(html);
+
+    /*
+        If answer has already been made, show it (for ex. in case of Prev/Next navigation)
      */
     if(answers[currentQuestion]) {
-        var id = '#' + answers[currentQuestion];
-        $(id).attr('checked', 'checked');
+        var id = answers[currentQuestion];
+        document.getElementById(id).setAttribute('checked', 'checked');
+        // jQuery: $('#' + id).attr('checked', 'checked');
     }
 
     /*
-        Handle choice selection
+        Record the choice selection as an answer
      */
+    var nodes = document.querySelectorAll('input[name=choices]');
+
+    function makeClickHandler(el) {
+        return function() {
+            answers[currentQuestion] = el;
+        };
+    }
+
+    for(var i = 0; i < nodes.length; i++) {
+        nodes[i].addEventListener('click', makeClickHandler(nodes[i].id));
+    }
+
+    /* jQuery:
     $('input[name=choices]').click(function(e){
         answers[currentQuestion] = e.target.id;
     });
+    */
 
 }
 
-
-/*
-    On document ready
- */
-$(function(){
+function init() {
 
     var currentQuestion = 0;
     var quizData;
 
+    /*
+     Create Handlebars
+     */
+
+    var source = document.getElementById('quiz-template').innerHTML;
+    // jQuery: var source = $('#quiz-template').html();
+    var template = Handlebars.compile(source);
+
 
     /*
-        Get the quiz data
+         Get the quiz data
      */
-    $.getJSON('data/australian-citizenship.json', function(data) {
+    function reqListener() {
+        quizData = this.responseText;
+        quizData = JSON.parse(quizData);
+        askQuestion(quizData, template, currentQuestion);
+    }
+
+    var oReq = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    oReq.open('get', 'data/australian-citizenship.json', true);
+    oReq.send();
+    /* jQuery:
+    $.getJSON('data/australian-citizenship.json', function (data) {
         quizData = data;
-        askQuestion(quizData, currentQuestion);
+        askQuestion(quizData, template, currentQuestion);
     });
-
+    */
 
     /*
-        Register click event for previous and next buttons
+     Register click event for previous and next buttons
      */
-    $('#prevButton, #nextButton').on('click', function(e) {
+    document.addEventListener('click', function(e) {
+    // jQuery: $('#prevButton, #nextButton').on('click', function (e) {
 
         /*
-            Check if question has been answered
+         Check if question has been answered
          */
-        if(e.target.id === 'nextButton' && !answers[currentQuestion]) {
-            $('#error').remove();
-            $('#quiz').append('<p id="error">Please, answer this question first!</p>');
+        if (e.target.id === 'nextButton' && !answers[currentQuestion]) {
+            var elRemove = document.getElementById('remove');
+            if (elRemove !== null) {
+                elRemove.parentNode.removeChild(elRemove);
+            }
+
+            var elError = document.createElement('p');
+            elError.setAttribute('id', 'remove');
+            elError.appendChild(document.createTextNode('Please, answer this question first!'));
+            document.getElementById('quiz').appendChild(elError);
+            // jQuery:
+            // $('#error').remove();
+            // $('#quiz').append('<p id="error">Please, answer this question first!</p>');
             return;
         }
 
         /*
-            Handle Next/Previous buttons
+         Handle Next/Previous buttons
          */
-        if(e.target.id === 'nextButton' && quizData[currentQuestion + 1]) {
+        if (e.target.id === 'nextButton' && quizData[currentQuestion + 1]) {
             currentQuestion++;
-            askQuestion(quizData, currentQuestion);
-        } else if(e.target.id === 'prevButton' && quizData[currentQuestion - 1]) {
+            askQuestion(quizData, template, currentQuestion);
+        } else if (e.target.id === 'prevButton' && quizData[currentQuestion - 1]) {
             currentQuestion--;
-            askQuestion(quizData, currentQuestion);
+            askQuestion(quizData, template, currentQuestion);
         }
 
         /*
-            Enable/Disable buttons at the start/end
+         Enable/Disable buttons at the start/end
          */
-        switch(currentQuestion) {
+        switch (currentQuestion) {
             //the first
             case 0:
-                $('#prevButton').attr('disabled', 'disabled');
+                document.getElementById('prevButton').setAttribute('disabled', 'disabled');
+                // jQuery: $('#prevButton').attr('disabled', 'disabled');
                 break;
 
             //the last
             case quizData.length - 1:
-                $('#nextButton').attr('disabled', 'disabled');
+                document.getElementById('nextButton').setAttribute('disabled', 'disabled');
+                // jQuery: $('#nextButton').attr('disabled', 'disabled');
                 break;
 
             //somewhere in between
             default:
-                $('#prevButton, #nextButton').removeAttr('disabled');
+                document.getElementById('prevButton').removeAttribute('disabled');
+                document.getElementById('nextButton').removeAttribute('disabled');
+                //$('#prevButton, #nextButton').removeAttr('disabled');
 
         }
     });
 
-});
+}
+
+window.onload = function() {
+    init();
+};
+
+//    jQuery: On document ready
+//    $(function() {});
